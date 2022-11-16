4ab0bba7dfe0dc46404f71ee93a167e4fc4d5174
Mustafa Çiçek <mustafa06cck@gmail.com>
2022-11-17 02:10:46 +0300

    uıapplication top view controller extension added.

---
+++ Company-Manage/Company-Manage/Managers/Alert/AlertError.swift
@@ -0,0 +1,24 @@
+//
+//  AlerError.swift
+//  Company-Manage
+//
+//  Created by Mustafa Çiçek on 17.11.2022.
+//
+
+import Foundation
+
+enum AlertError: Error {
+    case emptyInput
+    case wrongInput
+}
+
+extension AlertError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .emptyInput:
+            return "Value cannot be blank. Please enter a value."
+        case .wrongInput:
+            return "Please enter a valid value."
+        }
+    }
+}
---
+++ Company-Manage/Company-Manage/Managers/Alert/AlertManager.swift
@@ -0,0 +1,33 @@
+//
+//  AlertManager.swift
+//  Company-Manage
+//
+//  Created by Mustafa Çiçek on 17.11.2022.
+//
+
+import UIKit
+
+
+
+
+
+protocol AlertShowable {
+    func showAlert(with error: AlertError)
+}
+
+
+final class AlertManager: AlertShowable {
+    static let shared: AlertManager = .init()
+    
+    func showAlert(with error: AlertError)  {
+        let alert = UIAlertController(title: "Opps!!", message: error.description, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        
+        DispatchQueue.main.async {
+            UIApplication.
+        }
+    }
+}
+
+
